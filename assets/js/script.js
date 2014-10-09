baf024deee739139a0dabb71f06df5f74fceb67c
Kiran Panesar <kiran@mobilexlabs.com>
2014-10-08 23:41:21 -0500

    Set stars background to use parallax

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#title-content-container').plaxify({"xRange":20,"yRange":20})
+	$('.background-container').plaxify({"xRange":5,"yRange":5})
 
 	$.plax.enable()
 });
