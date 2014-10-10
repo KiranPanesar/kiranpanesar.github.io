70c719319a9f7cde0f9ad04544c0bf247f51e386
Kiran Panesar <kiran@mobilexlabs.com>
2014-10-09 19:30:48 -0500

    Added animated scroll down arrow

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
 	$('.background-container').plaxify({"xRange":5,"yRange":5})
 
 	$.plax.enable()
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
 });
